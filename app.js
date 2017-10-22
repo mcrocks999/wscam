cc85420302ef90e2284624ed26b61e2046b69324
Paul Makles <paulmakles@gmail.com>
2017-10-22 18:43:05 +0100

    Added capturing and sending, everything works now.

--- app.js
+++ app.js
@@ -2,4 +2,27 @@ const express = require('express'), http = require('http'), WebSocket = require(
 app = express(), server = http.createServer(app), wss = new WebSocket.Server({server, path:'/ws'}),
 nodewebcam = require('node-webcam'), webcam = nodewebcam.create({output:'jpeg',callbackReturn:'base64'});
 app.use(express.static(__dirname + '/public'));
+var frame = '';
+wss.on('connection', function(ws, req) {
+    if(frame!='') ws.send(JSON.stringify({type: "image", data: frame}));
+    console.log(req.connection.remoteAddress + " connected.");
+    ws.on('close', function(code, reason) {
+        console.log(req.connection.remoteAddress + " disconnected with reason " + code + ":" + reason);
+    })
+});
+function c() {
+    if (wss.clients.size>0) {
+        webcam.capture('o', (err, data) => {
+            if (err) return console.log(err);
+            frame = data;
+            wss.clients.forEach(function(ws) {
+                ws.send(JSON.stringify({type: "image", data: frame}));
+            });
+        });
+    }
+};
+c();
+setInterval(() => {
+    c();
+},4000);
 server.listen(3000,()=>{console.log("Listening on :3000")});
--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById('canvas'), context = canvas.getContext('2d'), updateFrame = true;
-var frame = 'iVBORw0KGgoAAAANSUhEUgAAADIAAAAPCAAAAACDsMMeAAABWklEQVQoz63STSjDARjH8V95OThJcpZyYQ5WkoP0nxtFTCEu8hKym7U4KMtLSmqWkgMuFkkKeXfBzct1orTTZjh4Kfsr7eswa3ZDntOvnj6H50X8uvS/JDvz16TGSOSltx+RigR50Ml3MnF5NDBlAqy4xuKdoGd4t8CAgNcxHeLKqW5PgINx5wIIrOWSLBAqlqRFAB7zJWUYBFM0bMt+2rersu7GIzlzWhBYS0Ls6ZJ+C9BZBIA7H/xZBvjhWVvc6RxMoDcNgdUNEe1R5gDW9QJQ3xUf/3rLK1+MwMVyj0yBdQSi2iF3EDhUAKC0C6g1oEWtPXHiL1RnZYyMQlTb2JqA+XQAqu0xsqZbkI+wzqG1CjaTyGzqJqulQwDM6ZhJGWxomxn5+FBHhLZik+YkwmCG0vq+ltwuNbgMqJYa5YMO6fQsTxqSmXzKYCJ+vMZW9PL+1QoD9398y085E1ZIUYf1aAAAAABJRU5ErkJggg==';
+var frame = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAPCAAAAACDsMMeAAABWklEQVQoz63STSjDARjH8V95OThJcpZyYQ5WkoP0nxtFTCEu8hKym7U4KMtLSmqWkgMuFkkKeXfBzct1orTTZjh4Kfsr7eswa3ZDntOvnj6H50X8uvS/JDvz16TGSOSltx+RigR50Ml3MnF5NDBlAqy4xuKdoGd4t8CAgNcxHeLKqW5PgINx5wIIrOWSLBAqlqRFAB7zJWUYBFM0bMt+2rersu7GIzlzWhBYS0Ls6ZJ+C9BZBIA7H/xZBvjhWVvc6RxMoDcNgdUNEe1R5gDW9QJQ3xUf/3rLK1+MwMVyj0yBdQSi2iF3EDhUAKC0C6g1oEWtPXHiL1RnZYyMQlTb2JqA+XQAqu0xsqZbkI+wzqG1CjaTyGzqJqulQwDM6ZhJGWxomxn5+FBHhLZik+YkwmCG0vq+ltwuNbgMqJYa5YMO6fQsTxqSmXzKYCJ+vMZW9PL+1QoD9398y085E1ZIUYf1aAAAAABJRU5ErkJggg==';
 function setFrame(base64) {frame=base64;updateFrame=true;}
-function draw() {var image=new Image();image.onload=function() {context.drawImage(image, 0, 0);};image.src='data:image/png;base64,'+frame}
+function draw() {var image=new Image();image.onload=function() {context.drawImage(image, 0, 0);};image.src=frame}
 function anim() {if (updateFrame) {draw();updateFrame=false}requestAnimationFrame(anim)}anim();
 var socket;if(window.location.protocol === "https://") {socket=new WebSocket("wss://" + window.location.host + "/ws")}
 else {socket=new WebSocket("ws://"+window.location.host+"/ws")}
